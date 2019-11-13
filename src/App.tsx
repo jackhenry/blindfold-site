8403574e13d6572c41a44b8b47586320a831e9b3
Jack Erickson <jackhenry.tech@gmail.com>
2019-11-12 19:05:12 -0600

    Added Scoreboard route and ApolloProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,30 @@ import { Provider } from "react-redux";
 import { Router, Route, Switch } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
 
-import HomePage from "./components/home/HomePage";
-import history from "./util/history";
+import { client } from "./util/gqlclient";
 import { store } from "./redux/store";
+import history from "./util/history";
+
+import HomePage from "./components/home/HomePage";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ScoresView from "./components/scoreboard/view/ScoresView";
+import TeamProvider from "./providers/TeamProvider";
 
 function App() {
   return (
     <div>
       <CssBaseline />
       <Provider store={store}>
-        <Router history={history}>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-          </Switch>
-        </Router>
+        <ApolloProvider client={client}>
+          <TeamProvider>
+            <Router history={history}>
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/scoreboard" component={ScoresView} />
+              </Switch>
+            </Router>
+          </TeamProvider>
+        </ApolloProvider>
       </Provider>
     </div>
   );
