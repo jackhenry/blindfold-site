03d589c2b7f04a70d72e493344f13a6f01ff7f4e
Jack Erickson <jackhenry.tech@gmail.com>
2019-11-15 21:48:14 -0500

    Created supplementary league data.

---
+++ src/data/leagues.ts
@@ -0,0 +1,19 @@
+const Leagues = [
+  {
+    label: "NFL",
+    value: "NFL",
+    image: "https://i.postimg.cc/Nf7xHS7H/nfl.png"
+  },
+  {
+    label: "NBA",
+    value: "NBA",
+    image: "https://i.postimg.cc/rFLJzMWT/nba.png"
+  },
+  {
+    label: "NHL",
+    value: "NHL",
+    image: "https://i.postimg.cc/WbFvpmMC/nhlblack.png"
+  }
+];
+
+export default Leagues;
