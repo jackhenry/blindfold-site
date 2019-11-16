a10959a7fe2268abf3d12b54c84cc0e1103c0ee5
Jack Erickson <jackhenry.tech@gmail.com>
2019-11-15 21:42:18 -0500

    Removal of unused modules

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-import Suggestions from "../suggestions/Suggestions";
-
-import AutocompleteInput from "../suggestions/AutocompleteInput";
 import ScoreFilter from "./ScoreFilter";
 
 const useStyles = makeStyles(theme => ({
